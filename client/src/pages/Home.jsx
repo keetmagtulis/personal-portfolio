a50ed446c37ad592d33910d353b62ec6573db1e7
Kit Magtulis <102131789+keetmagtulis@users.noreply.github.com>
2024-08-08 12:06:33 +0000

    Add PDF File

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 import '../../src/styles/index.css'; // Ensure this file imports Tailwind CSS correctly
 
+
+
+
 const Home = () => {
+
+  const onButtonClick = () => {
+    const pdfUrl = "../Kit _Magtulis_CV.pdf";
+    const link = document.createElement("a");
+    link.href = pdfUrl;
+    link.download = "Kit_Magtulis_CV.pdf"; // specify the filename
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+};
+
+
   return (
     
       <div className="flex items-center justify-center space-x-16 rounded-lg mx-auto w-9/12 h-5/6 mt-1">
@@ -16,7 +31,7 @@ const Home = () => {
           <h1 className="text-7xl mb-2">DEVELOPER</h1>
           <p>Hi, I’m Kit. I’m an IT Professional with 2 years of experience focusing on IT and Technical Support, Network Administration, Active Directory Management, and Software Development.</p>
 
-          <button className="bg-red-700 px-6 py-2 mt-9 rounded-full flex items-center justify-center">Download CV <img src="/images/download-2-24.ico" alt="Download" className="w-3 ml-3 flex items-center justify-center"></img></button>
+          <button className="bg-red-700 px-6 py-2 mt-9 rounded-full flex items-center justify-center" onClick={onButtonClick}>Download CV <img src="/images/download-2-24.ico" alt="Download" className="w-3 ml-3 flex items-center justify-center"></img></button>
 
         </div>
 
