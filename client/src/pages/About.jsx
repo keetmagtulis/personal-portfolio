82fd790ba9d2b6fbdb90b31186415107c7c862f0
Kit Magtulis <102131789+keetmagtulis@users.noreply.github.com>
2024-08-28 12:23:20 +0000

    Added vertical line

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -8,14 +8,14 @@ const About= () => {
 
       <div className="gap-20 mx-auto w-2/3">
       
-      <div className="col-span-3">
+      <div className="">
         <h2 className="text-gray-800 text-5xl font-bold my-5">ABOUT ME</h2>
         <p className="text-lg">My name is Kit, and I’m a guy with a deep curiosity for technology and how things work. I love exploring new innovations and constantly learning about the latest trends. Outside of tech, I enjoy going out, playing games, strumming on my guitar, and diving into good books. Whether it’s understanding the latest gadget or simply enjoying my hobbies, I’m always eager to explore and learn something new. </p>
       </div>
 
-      <div className="grid grid-cols-2 mx-auto w-2/3 mt-10">
+      <div className="grid grid-cols-3 mx-auto w-2/3 mt-10">
 
-      <div className=" flex flex-col">
+      <div className="flex flex-col">
         <h2 className="text-gray-800 text-4xl font-bold mb-2">SKILLS</h2>
         <p className="text-lg my-1">Network Administration</p>
         <p className="text-lg my-1">Active Directory Management</p>
@@ -28,8 +28,13 @@ const About= () => {
         <p className="text-lg my-1">Tailwind / CSS / Bootstrap</p>
         <p className="text-lg my-1">SQL</p>
       </div>
+
+      <div
+      className="inline-block ml-40 h-[400px] min-h-[1em] w-0.5 self-stretch bg-neutral-500 dark:bg-white/10">
+      </div>
+
       <div className="flex flex-col">
-        <h2 className="text-gray-800 text-4xl font-bold mb-2">WORK EXPERIENCE</h2>
+        <h2 className="text-gray-800 text-4xl font-bold mb-2 ">WORK EXPERIENCE</h2>
         <p>2023 - Present</p>
         <h3>Junior Technical Support Analyst</h3>
         <p>Philippine EDS Techno-Service, Inc.</p>
