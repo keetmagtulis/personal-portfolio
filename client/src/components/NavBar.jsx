5d6318024ab23d6a33fe2db408a85aeef1c0ba87
Kit Magtulis <102131789+keetmagtulis@users.noreply.github.com>
2024-09-19 11:22:58 +0000

    edited nav bar design

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -63,27 +63,28 @@ const NavBar = () => {
   return (
 
     <motion.nav
+      id="home"
       initial={{ y: -250 }}
       animate={{ y: 0 }}
       transition={{ delay: 0.5 }}
       className="flex w-full mx-auto py-4 items-center top-0"
-      id="home"
+      
     >
       {/* Centered Navigation Links */}
       <div
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
-      className={`fixed left-1/2 transform -translate-x-1/2 flex space-x-4 bg-neutral-200 shadow-lg rounded-full p-3 z-40  transition-opacity duration-500 ${
-        isVisible ? "opacity-100" : "opacity-0"
+      className={`fixed left-1/2 transform -translate-x-1/2 flex space-x-4 bg-neutral-900 shadow-lg rounded-full px-6 p-2 z-40  transition-opacity duration-500 mt-2 mx-2 ${
+        isVisible ? "opacity-90" : "opacity-0"
       }`}
       >
         <Link
-          to="home"
+          to='home'
           spy={true}
           smooth={true}
-          offset={0}
           duration={500}
-          className="cursor-pointer text-neutral-700 px-2 py-2 rounded-md text-s font-semibold"
+          offset={0}
+          className="cursor-pointer text-neutral-200 px-2 py-2 rounded-md text-s font-normal"
         >
           <motion.div
             whileHover={{ scale: 1.05}}
@@ -97,12 +98,12 @@ const NavBar = () => {
           to="about"
           spy={true}
           smooth={true}
-          offset={-70}
+          offset={-20}
           duration={500}
-          className="cursor-pointer text-neutral-700 px-2 py-2 rounded-md text-s font-semibold"
+          className="cursor-pointer text-neutral-200 px-2 py-2 rounded-md text-s font-normal"
         >
           <motion.div
-            whileHover={{ scale: 1.05}}
+            whileHover={{ scale: 1.05,}}
             whileTap={{ scale: 0.9 }}
             
           >ABOUT
@@ -113,9 +114,9 @@ const NavBar = () => {
           to="projects"
           spy={true}
           smooth={true}
-          offset={-70}
+          offset={-10}
           duration={500}
-          className="cursor-pointer text-neutral-700  px-2 py-2 rounded-md text-s font-semibold"
+          className="cursor-pointer text-neutral-200  px-2 py-2 rounded-md text-s font-normal"
         >
 
           <motion.div
@@ -128,7 +129,7 @@ const NavBar = () => {
         </Link>
         <RouterLink
           to="/socials"
-          className="text-neutral-700  px-2 py-2 rounded-md text-s font-semibold"
+          className="text-neutral-200  px-2 py-2 rounded-md text-s font-normal"
         >
           <motion.div
             whileHover={{ scale: 1.05}}
@@ -142,33 +143,33 @@ const NavBar = () => {
 
       { /*Soc Med Contact Icons*/}
       <div className="ml-auto flex items-center mr-7">
-        <RouterLink to="https://github.com/keetmagtulis">
+        <RouterLink to="https://github.com/keetmagtulis" target="_blank">
           <motion.img
-            whileHover={{ scale: 1.1}}
+            whileHover={{ scale: 1.3}}
             whileTap={{ scale: 0.9 }}
             transition={{ type: "spring", stiffness: 400, damping: 17 }}
           src="/images/github-sign.png" alt="" className="w-7 h-7 mx-2" />
         </RouterLink>
 
-        <RouterLink to="https://www.linkedin.com/in/kit-magtulis-0532362b7/">
+        <RouterLink to="https://www.linkedin.com/in/kit-magtulis-0532362b7/" target="_blank">
           <motion.img 
-           whileHover={{ scale: 1.1}}
+           whileHover={{ scale: 1.3}}
            whileTap={{ scale: 0.9 }}
            transition={{ type: "spring", stiffness: 400, damping: 17 }}
           src="/images/linkedin.png" alt="Github" className="w-7 h-7 mx-2" />
         </RouterLink>
         
-        <RouterLink to="mailto:magtuliskit8@gmail.com">
+        <RouterLink to="mailto:magtuliskit8@gmail.com" target="_blank">
           <motion.img 
-           whileHover={{ scale: 1.1}}
+           whileHover={{ scale: 1.3}}
            whileTap={{ scale: 0.9 }}
            transition={{ type: "spring", stiffness: 400, damping: 17 }}
           src="/images/email.png" alt="" className="w-7 h-7 mx-2" />
         </RouterLink>
         
-        <RouterLink to="https://www.messenger.com/t/100054884522654">
+        <RouterLink to="https://www.messenger.com/t/100054884522654" target="_blank">
           <motion.img 
-           whileHover={{ scale: 1.1}}
+           whileHover={{ scale: 1.3}}
            whileTap={{ scale: 0.9 }}
            transition={{ type: "spring", stiffness: 400, damping: 17 }}
           src="/images/facebook.png" alt="" className="w-7 h-7 mx-2" />
