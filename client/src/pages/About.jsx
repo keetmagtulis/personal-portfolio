c26e55a9eff75f15a24e628c7f20a56abb3cd4bd
Kit Magtulis <102131789+keetmagtulis@users.noreply.github.com>
2024-10-01 12:28:58 +0000

    fix: code clean up and removed select element in about me card

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -25,21 +25,18 @@ const scrollRef = useRef(null);
             <motion.div 
               layoutId="aboutme"
               initial= {{ opacity: 0}}
-              whileHover={{ scale: 1.02}}
-              whileTap={{scale: 0.8}}
               whileInView={{opacity: 1, transition: {
                 delay: 0.3,
                 duration: 1,
               }}}
               viewport={{root: scrollRef}}
               transition={{ type: "delay"}}
-              onClick={() => setSelectedId("aboutme")}
                   
-              className="bg-white rounded-lg shadow-md p-6 cursor-pointer">
+              className="bg-white rounded-lg shadow-md p-6">
               
                   {/* Cover Image */}
                   <img
-                    src="/images/about-me-background2.jpg"
+                    src="/images/about-me-background.jpg"
                     alt="Profile"
                     className="w-full h-52 object-cover rounded-t-md mb-4"
                   />
@@ -68,7 +65,7 @@ const scrollRef = useRef(null);
 
             </motion.div>
           
-            <div>
+        <div>
             {/* Skills */}
             <motion.div 
               layoutId="skills"
@@ -142,16 +139,13 @@ const scrollRef = useRef(null);
                 </motion.button>
 
             </motion.div>
-
-            </div>
-
-            
-    
         </div>
 
-    <AnimatePresence>
-        {selectedId && (
-          <>
+        </div>
+
+      <AnimatePresence>
+        {selectedId && (   
+        <div>
             {/* Blur Background */}
             <motion.div
               initial={{ opacity: 0 }}
@@ -166,30 +160,6 @@ const scrollRef = useRef(null);
               layoutId={selectedId}
               className="fixed inset-0 bg-white p-5 flex flex-col items-center justify-center rounded-md shadow-lg w-10/12 h-5/6 lg:w-1/2 max-w-2xl mx-auto my-auto z-50 "
             >
-            {selectedId === "aboutme" && (
-                <div className="flex flex-col items-center">
-                  
-             
-
-                  {/* About Me Content with Scroll */}
-                  <div className="scrollbar-DEFAULT overflow-y-auto max-h-96 p-4 w-full">
-                  
-                  
-
-                    <h2 className="text-gray-800 text-4xl font-semibold mb-4 border-b-2 border-red-700 pb-2 drop-shadow-lg">
-                      ABOUT ME
-                    </h2>
-                    <p className="text-lg">
-                    My name is Kit, and I’m a guy with a deep curiosity for technology and how things work. I love exploring new innovations, whether it’s the latest software, network configurations, or discovering how different technologies interact. With a background in IT, I have hands-on experience in network administration, database management, Active Directory, and software development, which allows me to tackle a wide range of technical challenges.
-                    </p>
-                    <p className="text-lg"> Outside of my professional interests, I enjoy going out, playing games, strumming my guitar, and diving into good books. I believe that a balance of work and hobbies fuels my creativity and keeps me motivated. Whether it’s understanding the latest tech trends or enjoying my favorite pastimes, I’m always eager to explore, learn, and grow.
-                    </p>
-                   
-
-                    
-                  </div>
-                </div>
-            )}
                                 
             {selectedId === "skills" && (
              <div  className="w-full max-w-lg" >
@@ -364,12 +334,12 @@ const scrollRef = useRef(null);
                 onClick={() => setSelectedId(null)}>Close</motion.button>
             
             </motion.div>
-          </>
+        </div>
         )}
       </AnimatePresence>
 
-
-    </div>
+  
+  </div>
 
       
     
