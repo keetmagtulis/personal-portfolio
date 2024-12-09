5f7c24f2dfa8da69cb4d1a8183ca61d3cbbd684e
Kit Magtulis <magtuliskit8@gmail.com>
2024-12-09 21:46:11 +0800

    Test commit for local clone, updated facebook link

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -146,7 +146,7 @@ const NavBar = () => {
           src="/images/email.png" alt="" className="w-7 h-7 " />
         </RouterLink>
         
-        <RouterLink to="https://www.messenger.com/t/100054884522654" target="_blank">
+        <RouterLink to="https://www.facebook.com/kitdominic.magtulis" target="_blank">
           <motion.img 
            whileHover={{ scale: 1.3}}
            whileTap={{ scale: 0.9 }}
